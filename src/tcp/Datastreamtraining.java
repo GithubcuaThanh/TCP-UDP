2d3bc8298dca4253caad31213d99ff6e8ce73561
bieenddi <k18a.vietthanh@gmail.com>
2025-05-14 07:22:10 +0700

    on tap

---
+++ src/tcp/Datastreamtraining.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tcp;
+
+import java.util.*;
+import java.io.*;
+import java.net.*;
+/**
+ *
+ * @author LaptopcuaThanh
+ */
+public class Datastreamtraining {
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("203.162.10.109",2207);
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        
+        String ma = "B19DCCN651;lmyLzvUj";         
+        out.writeUTF(ma);
+        out.flush();
+
+        int a = in.readInt();
+        int b = in.readInt();
+        
+        System.out.println(a + b);
+
+
+        int tong = a+b;
+        int tich = a*b;
+
+        out.writeInt(tong);        
+        out.writeInt(tich);
+        
+        in.close();
+        out.close();
+        socket.close();
+    }
+}
